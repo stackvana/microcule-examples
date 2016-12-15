fd6fd383ca0ac38606b885866a351ea272112827
Marak <marak.squires@gmail.com>
2016-12-14 22:49:14 -0500

    [api] Added babel examples

---
+++ babel/index.js
@@ -0,0 +1,8 @@
+// A simple hello world microservice 
+// Click "Deploy Service" to deploy this code
+// Service will respond to HTTP requests with a string
+import sleep from 'then-sleep';
+export default async function (hook) {
+  await sleep(500);
+  hook.res.json(hook.params);
+}
