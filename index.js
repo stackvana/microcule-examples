fc287ce9baa03a8f1cbe8191f7461dd4d1e984bd
Marak <marak.squires@gmail.com>
2016-12-22 18:08:57 -0500

    [api] [fix] Add `.npmignore` to exclude list

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var dir = fs.readdirSync(__dirname);
 
 // filter directories for any known non-service folders
 dir = dir.filter(function(item){
-  if ([".git", "node_modules", ".DS_Store", ".gitignore", "ReadMe.md", "package.json", "index.js"].indexOf(item) === -1) {
+  if ([".git", "node_modules", ".DS_Store", ".gitignore", "ReadMe.md", "package.json", "index.js", ".npmignore"].indexOf(item) === -1) {
     return true;
   }
 });
